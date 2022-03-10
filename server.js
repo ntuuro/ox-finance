c336cc69f2800c4cd7c5e838a581e5f041c623c7
Elijah97 <elie.gash42@gmail.com>
2022-03-10 12:13:45 +0200

    Cors edited

--- server.js
+++ server.js
@@ -12,20 +12,13 @@ app.use(
 );
 
 // Setting up corsOptions
-var corsOptions = {
-  origin: ["http://localhost:8081", "http://localhost:3000", "*"],
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
 };
 
-app.use(cors(corsOptions));
-
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+app.use(cors(corsOptions)); // Use this after the variable declaration
 
 // parse requests of content-type - application/json
 app.use(express.json());
