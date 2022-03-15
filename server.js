2d3baf6035f8a4dfc3c5f70340d4f995b3320796
Elijah97 <elie.gash42@gmail.com>
2022-03-15 14:30:45 +0200

    Returned to one cors

--- server.js
+++ server.js
@@ -14,19 +14,8 @@ app.use(
 );
 
 // Setting up corsOptions
-// var corsOptions = {
-//   origin: "http://localhost:3000",
-//   optionsSuccessStatus: 200
-// };
-var whitelist = ["http://localhost:3000"];
 var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: "http://localhost:3000",
 };
 
 app.use(cors(corsOptions));
