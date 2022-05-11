1f68c9578e98deecc9cf806e85ceb8bd24da6658
Elijah97 <elie.gash42@gmail.com>
2022-05-11 18:46:49 +0200

    Added new urls

--- server.js
+++ server.js
@@ -13,11 +13,7 @@ app.use(
 
 // Setting up corsOptions
 var corsOptions = {
-  origin: [
-    "http://localhost:3000",
-    "https://dev-api.ox.rw/api",
-    "https://dev-finance-api.ox.rw/",
-  ],
+  origin: ["http://localhost:3000", "https://dev-finance.ox.rw"],
 };
 
 app.use(cors(corsOptions));
